603b94e71d2efc9f60d96a573cc14167a53ab8b8
NStriebel <StriebelNJ21@gcc.edu>
2022-11-09 13:41:15 -0500

    Added empty ConsoleDriver class

--- src/ConsoleDriver.java
+++ src/ConsoleDriver.java
@@ -1,5 +1,19 @@
 public class ConsoleDriver {
     public static void main(String[] args) {
-        //This is
+        //bulk of logic elsewhere
+        //this should only have I/O logic
+        //meaning talk to user, get user input, show changes to user
+
+        //eg
+        //create an object of type Game and call its methods
+        //Game g = new Game();
+        //while(g.notDone(){
+        //  ask user for input
+        //  g.displayStatus()
+        //}
+
+        //this lets you add a gui with minimal changes to game logic if any
+
+        //the design needs main components, but not necessarily every class.
     }
 }
