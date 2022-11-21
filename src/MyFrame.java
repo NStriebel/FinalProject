0905506187b952621c0c2ee159a5077e3fe502d4
LIPNICHANRT21 <lipnichanrt21>
2022-11-21 13:05:43 -0500

    Creating a new java class to create the window that the game of Atari Breakout will be played.

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,2 +1,12 @@
-public class MyFrame {
+import javax.swing.*;
+import java.awt.event.KeyListener;
+
+public class MyFrame extends JFrame {
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+        this.setVisible(true);
+    }
 }
