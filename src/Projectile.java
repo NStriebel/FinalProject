27dc548b22c1c6fe3f4a4f3df37ab45358dc3c62
LIPNICHANRT21 <lipnichanrt21>
2022-12-02 09:01:58 -0500

    Started adding JavaDoc to methods in Projectile class.

--- src/Projectile.java
+++ src/Projectile.java
@@ -6,6 +6,16 @@ import java.awt.*;
 public class Projectile extends Drawable {
     private int radius;
 
+    /**
+     * This constructor takes in the radius, x positon, y position, velocities in the x and y directions,
+     * and the color of the Projectile object to initialize a Projectile object.
+     * @param radius the radius of the Projectile
+     * @param xPos the x position of the Projectile
+     * @param yPos the y position of the Projectile
+     * @param xVel the velocity in the x direction of the Projectile
+     * @param yVel the velocity in the y direction of the Projectile
+     * @param col the color of the Projectile
+     */
     public Projectile(int radius, double xPos, double yPos, double xVel, double yVel, Color col){
         super(xPos,yPos,xVel,yVel,col);
         this.radius = radius;
