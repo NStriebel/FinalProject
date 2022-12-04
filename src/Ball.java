c3288e7f1df3af5436f8d28b40b8aca4bfbeb2a5
LIPNICHANRT21 <lipnichanrt21>
2022-12-03 19:49:22 -0500

    Started adding JavaDoc to the Ball constructor in the Ball class.

--- src/Ball.java
+++ src/Ball.java
@@ -4,6 +4,15 @@ import java.awt.*;
  * This class represents a ball that bounces around the screen and decrements bricks' durabilites when it collides with them.
  */
 public class Ball extends Projectile{
+    /**
+     * This constructor takes in the radius of the ball, the x and y positions of the ball, and the
+     * x and y velocities of the ball to initialize the ball that will be used in the game.
+     * @param rad the radius of the ball.
+     * @param xPos the x position of the ball.
+     * @param yPos the y position of the ball.
+     * @param xVel the x velocity of the ball.
+     * @param yVel the y velocity of the ball.
+     */
     public Ball(int rad, double xPos, double yPos, double xVel, double yVel){
         super(rad, xPos, yPos, xVel, yVel, new Color(224, 210, 160));
     }
