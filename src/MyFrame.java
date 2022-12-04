96d82e05fbcf80028335c1817c40ea6af49045d4
LIPNICHANRT21 <lipnichanrt21>
2022-12-03 19:52:12 -0500

    Started adding JavaDoc to the MyFrame constructor in the MyFrame class.

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,6 +4,11 @@ import java.awt.event.KeyListener;
 
 public class MyFrame extends JFrame{
 
+    /**
+     * This constructor creates the window in which the game will be played in. Inside this constructor,
+     * the size of the window is set, the layout is set, its visibility is set, and how the window closes
+     * is set.
+     */
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
